bfe043f3941ed140b6e95adc45a88cd3bf77748e
johnskyi <johnskyi2@yandex.ru>
2021-08-21 21:18:29 +0300

    Initial commit

---
+++ src/main/java/com/example/MoneyCalc/MoneyCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.MoneyCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MoneyCalc/MoneyCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MoneyCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
